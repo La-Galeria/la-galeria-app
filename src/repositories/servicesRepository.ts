234441f3373f4a3a4eb4faee7dc62e5b61aa290d
Manuel <github.splurge944@passinbox.com>
2025-07-12 19:03:23 -0500

    refactor: adapt services repo to class logic

--- src/repositories/servicesRepository.ts
+++ src/repositories/servicesRepository.ts
@@ -1,42 +1,14 @@
 import { ServiceDTO } from "@/dtos/serviceDto";
 import { Servicio } from "@/generated/prisma";
-import Repository from "@/interfaces/repository";
+import IRepository from "@/interfaces/IRepository";
 import prisma from "@/lib/prisma";
+import BaseRepository from "./baseRepository";
 
-interface ServiceRepo extends Repository<Servicio, ServiceDTO> { };
-
-async function get(id: number) {
-  return await prisma.servicio.findFirst({
-    where: {
-      id: id
-    }
-  });
+class ServiceRepo extends BaseRepository<Servicio, ServiceDTO> implements IRepository<Servicio, ServiceDTO> {
+  constructor() {
+    super(prisma.servicio);
+  }
 }
 
-async function getAll() {
-  return await prisma.servicio.findMany();
-}
-
-async function add(data: ServiceDTO) {
-  return await prisma.servicio.create({ data });
-}
-
-async function update(id: number, data: ServiceDTO) {
-  return await prisma.servicio.update({
-    where: {
-      id: id
-    },
-    data: data
-  });
-}
-
-async function remove(id: number) {
-  return await prisma.servicio.delete({
-    where: {
-      id: id
-    }
-  });
-}
-
-const serviceRepo: ServiceRepo = { get, getAll, add, update, remove };
-export default serviceRepo;
+const servicesRepo = new ServiceRepo();
+export default servicesRepo;
