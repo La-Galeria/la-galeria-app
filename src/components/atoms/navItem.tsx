d90c8bd37660146c60887150cc914c504b43e57e
Manuel <github.splurge944@passinbox.com>
2025-07-30 18:34:56 -0500

    feat: add active link check

--- src/components/atoms/navItem.tsx
+++ src/components/atoms/navItem.tsx
@@ -1,15 +1,20 @@
 import Link from "next/link"
-import React, { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 interface NavItemProps {
   route: string,
-  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>,
 }
 export default function NavItem({ route, Icon }: NavItemProps) {
+  const pathname = usePathname();
+  const active = pathname === route;
+  console.log(active);
+
   return (
     <Link href={route}>
-      <div className="p-2">
-      <Icon className="size-14" />
+      <div className={`p-2 ${active && "bg-slate-50"}`}>
+        <Icon className={`size-14 ${active ? "text-lime-950" : "text-slate-50"}`} />
       </div>
     </Link>
   );
--- src/components/molecules/navBar.tsx
+++ src/components/molecules/navBar.tsx
@@ -1,12 +1,13 @@
+"use client";
 import { GoHome } from "react-icons/go";
 import NavItem from "../atoms/navItem";
 import { MdLogout, MdOutlineInventory } from "react-icons/md";
 import { FaPlus } from "react-icons/fa6";
 import { FiScissors } from "react-icons/fi";
 
-export default function NavBar({ className }: { className: string }) {
+export default function NavBar() {
   return (
-    <nav className="bg-lime-950 fixed bottom-0 text-slate-50 flex justify-around w-full">
+    <nav className="bg-lime-950 fixed bottom-0 flex justify-between w-full">
       <NavItem route="/" Icon={GoHome} />
       <NavItem route="/inventario" Icon={MdOutlineInventory} />
       <NavItem route="/nuevo" Icon={FaPlus} />
