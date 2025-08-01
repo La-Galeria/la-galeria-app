c17c96378e959727c1a820835ccfa31855042cd2
Manuel <github.splurge944@passinbox.com>
2025-07-31 19:03:12 -0500

    feat: add inventory page

--- src/app/(app)/inventario/page.tsx
+++ src/app/(app)/inventario/page.tsx
@@ -1,5 +1,17 @@
+import ItemsList from "@/components/atoms/itemsList";
+import PageTitle from "@/components/atoms/pageTitle";
+import SubmoduleLink from "@/components/atoms/submoduleLink";
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
+import { MdOutlineStore } from "react-icons/md";
+
 export default function Page() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <PageTitle>Inventario</PageTitle>
+      <ItemsList>
+        <SubmoduleLink route="/inventario" text="Productos" InnerIcon={MdOutlineStore} />
+        <SubmoduleLink route="/inventario" text="Insumos" InnerIcon={HiOutlineSquares2X2} />
+      </ItemsList>
+    </>
   );
 }
