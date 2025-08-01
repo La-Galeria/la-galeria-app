1c8586c5e0e30b7ccf8009351af79f8d2636f8a0
Manuel <github.splurge944@passinbox.com>
2025-08-01 18:59:01 -0500

    fix: use cart as the new purchase icon

--- src/app/(app)/nuevo/page.tsx
+++ src/app/(app)/nuevo/page.tsx
@@ -1,7 +1,7 @@
 import ItemsList from "@/components/atoms/itemsList";
 import PageTitle from "@/components/atoms/pageTitle";
 import SubmoduleLink from "@/components/atoms/submoduleLink";
-import { MdCreditCard, MdOutlineShop } from "react-icons/md";
+import { MdCreditCard, MdOutlineShop, MdOutlineShoppingCart } from "react-icons/md";
 import { RiBillLine } from "react-icons/ri";
 
 export default function Page() {
@@ -9,7 +9,7 @@ export default function Page() {
     <>
       <PageTitle>Nuevo registro</PageTitle>
       <ItemsList>
-        <SubmoduleLink route="/nuevo/compra" text="Compra" InnerIcon={MdOutlineShop} />
+        <SubmoduleLink route="/nuevo/compra" text="Compra" InnerIcon={MdOutlineShoppingCart} />
         <SubmoduleLink route="/nuevo/venta" text="Venta" InnerIcon={MdCreditCard} />
         <SubmoduleLink route="/nuevo/gasto" text="Gasto" InnerIcon={RiBillLine} />
       </ItemsList>
