c552e026d0cf22800fec1d1a54055e5c7b718a34
Manuel <github.splurge944@passinbox.com>
2025-08-01 18:51:37 -0500

    feat: add new elements page

--- src/app/(app)/nuevo/page.tsx
+++ src/app/(app)/nuevo/page.tsx
@@ -1,5 +1,18 @@
+import ItemsList from "@/components/atoms/itemsList";
+import PageTitle from "@/components/atoms/pageTitle";
+import SubmoduleLink from "@/components/atoms/submoduleLink";
+import { MdCreditCard, MdOutlineShop } from "react-icons/md";
+import { RiBillLine } from "react-icons/ri";
+
 export default function Page() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <PageTitle>Nuevo registro</PageTitle>
+      <ItemsList>
+        <SubmoduleLink route="/nuevo/compra" text="Compra" InnerIcon={MdOutlineShop} />
+        <SubmoduleLink route="/nuevo/venta" text="Venta" InnerIcon={MdCreditCard} />
+        <SubmoduleLink route="/nuevo/gasto" text="Gasto" InnerIcon={RiBillLine} />
+      </ItemsList>
+    </>
   );
 }
