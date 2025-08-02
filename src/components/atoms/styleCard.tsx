cecb2e87e6c8607993b85e4d15ae24f81fbbc9b7
Manuel <github.splurge944@passinbox.com>
2025-08-01 19:45:44 -0500

    feat: add shadow and transparency to style card component

--- src/components/atoms/styleCard.tsx
+++ src/components/atoms/styleCard.tsx
@@ -8,7 +8,7 @@ export interface StyleCardProps {
 
 export default function StyleCard({ placeTagAtLeft, children, Icon }: StyleCardProps) {
   return (
-    <div className="shadow-md rounded-3xl bg-slate-50 mx-4 my-7 h-30">
+    <div className="shadow-xl rounded-3xl bg-slate-50/80 mx-4 my-7 h-30">
       <div className={`${placeTagAtLeft ? "float-left rounded-l-3xl" : "float-right rounded-r-3xl"} bg-lime-950 h-full w-3/12 flex flex-col justify-center items-center p-2`}>
         <Icon className="text-slate-50 size-10" />
       </div>
