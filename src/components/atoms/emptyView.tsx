84684c83c50909e895db9d5da47d6f01109ece49
Manuel <github.splurge944@passinbox.com>
2025-08-01 19:43:53 -0500

    feat: add empty view component

---
+++ src/components/atoms/emptyView.tsx
@@ -0,0 +1,18 @@
+import { FaPlus } from "react-icons/fa6"
+
+interface EmptyViewProps {
+  keyword: string
+};
+
+export default function EmptyView({ keyword }: EmptyViewProps) {
+  return (
+    <div className="rounded-3xl bg-slate-50/70 border-dashed border-4 border-orange-950 h-5/12 my-10 mx-2 p-5">
+      <div className="flex flex-col justify-around items-center h-full">
+        <h2 className="text-center">Todavía no tienes {keyword}</h2>
+        <div className="rounded-full border-dashed border-4 border-orange-950 p-5">
+          <FaPlus className="size-14 text-orange-950" />
+        </div>
+      </div>
+    </div>
+  );
+}
