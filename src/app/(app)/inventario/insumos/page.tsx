b5f92a46d5da8b9938d4cc840f286ed22b1b677b
Manuel <github.splurge944@passinbox.com>
2025-08-02 13:51:32 -0500

    feat: add assets page

---
+++ src/app/(app)/inventario/insumos/page.tsx
@@ -0,0 +1,11 @@
+import EmptyView from "@/components/atoms/emptyView";
+import PageTitle from "@/components/atoms/pageTitle";
+
+export default function Page() {
+  return (
+    <>
+      <PageTitle>Insumos</PageTitle>
+      <EmptyView keyword="insumos"></EmptyView>
+    </>
+  )
+}
