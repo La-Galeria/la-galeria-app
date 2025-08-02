4382bab02dec54352e47ce5d56ed51e6bd6f8e27
Manuel <github.splurge944@passinbox.com>
2025-08-02 17:55:03 -0500

    fix: replace pricetag rounding to 3xl

--- src/components/atoms/priceTag.tsx
+++ src/components/atoms/priceTag.tsx
@@ -5,7 +5,7 @@ interface PriceTagProps {
 export default function PriceTag({ price }: PriceTagProps) {
   const formattedPrice = Intl.NumberFormat("es-CO", { style: "currency", currency: "COP", maximumFractionDigits: 0 }).format(price);
   return (
-    <div className="absolute top-0 right-0 border-solid border-2 border-orange-950 rounded-bl-2xl rounded-tr-2xl">
+    <div className="absolute top-0 right-0 border-solid border-2 border-orange-950 rounded-bl-3xl rounded-tr-3xl">
       <div className="flex justify-center items-center py-0.5 px-3">
         <h4 className="underline">{formattedPrice}</h4>
       </div>
