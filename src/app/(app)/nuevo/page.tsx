20f58cb43dc920d90c903929551757141882632e
Manuel <github.splurge944@passinbox.com>
2025-08-02 13:56:39 -0500

    feat: add service, product and asset buttons to 'add new' page

--- src/app/(app)/nuevo/page.tsx
+++ src/app/(app)/nuevo/page.tsx
@@ -1,7 +1,9 @@
 import ItemsList from "@/components/atoms/itemsList";
 import PageTitle from "@/components/atoms/pageTitle";
 import SubmoduleLink from "@/components/molecules/submoduleLink";
-import { MdCreditCard, MdOutlineShop, MdOutlineShoppingCart } from "react-icons/md";
+import { FaScissors } from "react-icons/fa6";
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
+import { MdCreditCard, MdOutlineShop, MdOutlineShoppingCart, MdOutlineStore } from "react-icons/md";
 import { RiBillLine } from "react-icons/ri";
 
 export default function Page() {
@@ -12,6 +14,9 @@ export default function Page() {
         <SubmoduleLink route="/nuevo/compra" text="Compra" InnerIcon={MdOutlineShoppingCart} />
         <SubmoduleLink route="/nuevo/venta" text="Venta" InnerIcon={MdCreditCard} />
         <SubmoduleLink route="/nuevo/gasto" text="Gasto" InnerIcon={RiBillLine} />
+        <SubmoduleLink route="/nuevo/servicio" text="Servicio" InnerIcon={FaScissors} />
+        <SubmoduleLink route="/nuevo/producto" text="Producto" InnerIcon={MdOutlineStore} />
+        <SubmoduleLink route="/inventario/insumos" text="Insumo" InnerIcon={HiOutlineSquares2X2} />
       </ItemsList>
     </>
   );
