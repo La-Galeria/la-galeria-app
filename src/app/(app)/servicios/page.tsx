32b8192d9fceb5f7745234c995d6765167b008cf
Manuel <github.splurge944@passinbox.com>
2025-08-02 14:28:49 -0500

    chore: place service link into services page to test it out

--- src/app/(app)/servicios/page.tsx
+++ src/app/(app)/servicios/page.tsx
@@ -1,11 +1,16 @@
 import EmptyView from "@/components/atoms/emptyView";
+import ItemsList from "@/components/atoms/itemsList";
 import PageTitle from "@/components/atoms/pageTitle";
+import ServiceLink from "@/components/molecules/serviceLink";
 
 export default function Page() {
   return (
     <>
       <PageTitle>Servicios</PageTitle>
-      <EmptyView keyword="servicios" />
+      {/* <EmptyView keyword="servicios" /> */}
+      <ItemsList>
+        <ServiceLink name="Corte" price={10000} />
+      </ItemsList>
     </>
   );
 }
