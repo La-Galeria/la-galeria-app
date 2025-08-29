0580074072d1a51045e95e79274c178554253805
Manuel <github.splurge944@passinbox.com>
2025-08-28 19:58:05 -0500

    feat: add margin to action button

--- src/components/atoms/actionButton.tsx
+++ src/components/atoms/actionButton.tsx
@@ -4,7 +4,7 @@ interface ActionButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement
 
 export default function ActionButton({ className, children, ...props }: ActionButtonProps) {
   return (
-    <button className={"rounded-full px-10 py-2 " + className} {...props}>
+    <button className={"rounded-full px-10 py-2 m-2 " + className} {...props}>
       {children}
     </button>
   );
