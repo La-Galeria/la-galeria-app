192c79d0d841fc6825d82b9b17b7da2560ce6d40
Manuel <github.splurge944@passinbox.com>
2025-08-28 19:55:28 -0500

    feat: add full width to input field component

--- src/components/atoms/InputField.tsx
+++ src/components/atoms/InputField.tsx
@@ -2,18 +2,18 @@ interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 export default function InputField({ label, ...props }: InputFieldProps) {
-  const className = "border-solid border-orange-950 border rounded-2xl text-orange-950 text-lg p-2 my-0 w-full";
+  const className =
+    "border-solid border-orange-950 border rounded-2xl text-orange-950 text-lg p-2 my-0 w-full";
   return (
-    <div className="m-4">
+    <div className="w-full">
       <label>
         <h4>{label}</h4>
         {props.type == "textarea" ? (
-          <textarea className={className} rows={4}>{props.defaultValue}</textarea>
+          <textarea className={className} rows={4}>
+            {props.defaultValue}
+          </textarea>
         ) : (
-          <input
-            {...props}
-            className={className}
-          />
+          <input {...props} className={className} />
         )}
       </label>
     </div>
