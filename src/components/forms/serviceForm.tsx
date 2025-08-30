218ca97bd4f19332d1be86cad0b746f9f6209690
Manuel <github.splurge944@passinbox.com>
2025-08-30 09:04:07 -0500

    feat: add priceField component

--- src/components/forms/serviceForm.tsx
+++ src/components/forms/serviceForm.tsx
@@ -3,13 +3,14 @@ import GenericFormProps from "@/interfaces/genericFormProps";
 import Form from "next/form";
 import InputField from "../atoms/InputField";
 import FormWrapper from "./formWrapper";
+import PriceField from "../molecules/priceField";
 
 export default function ServiceForm({ entity, action }: GenericFormProps<Servicio>) {
   return (
     <FormWrapper action={action}>
       <InputField label="Nombre" />
       <InputField label="Descripción" type="textarea" />
-      <InputField label="Precio" />
+      <PriceField label="Precio" />
     </FormWrapper>
   );
 }
---
+++ src/components/molecules/priceField.tsx
@@ -0,0 +1,30 @@
+import { formatPrice, unformatPrice } from "@/utils/utils";
+import { ChangeEvent, useState } from "react";
+import InputField from "../atoms/InputField";
+
+interface PriceFieldProps {
+  initialValue?: number;
+  label: string;
+}
+
+export default function PriceField({ initialValue, label }: PriceFieldProps) {
+  const [value, setValue] = useState(initialValue ?? 0);
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>): void {
+    let inputValue = event.target.value;
+    console.log(`Received value: ${inputValue}`);
+
+    let value = unformatPrice(inputValue);
+    console.log(`Unformatted value: ${value}`);
+    setValue(value);
+  }
+
+  let displayValue = formatPrice(value);
+
+  return (
+    <>
+      <InputField label={label} onChange={handleChange} value={displayValue} />
+      <input hidden value={value} />
+    </>
+  );
+}
