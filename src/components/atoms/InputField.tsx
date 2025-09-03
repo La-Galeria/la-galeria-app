9d59eec7cadd2b8515dd4f03b6c1f36f301358bb
Manuel <github.splurge944@passinbox.com>
2025-09-03 18:39:47 -0500

    refactor: lift up input style to globals.css

--- src/components/atoms/InputField.tsx
+++ src/components/atoms/InputField.tsx
@@ -2,18 +2,16 @@ interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 export default function InputField({ label, ...props }: InputFieldProps) {
-  const className =
-    "border-solid border-orange-950 border rounded-2xl text-orange-950 text-lg p-2 my-0 w-full";
   return (
     <div className="w-full">
       <label>
         <h4>{label}</h4>
         {props.type == "textarea" ? (
-          <textarea className={className} rows={4} name={props.name}>
+          <textarea rows={4} name={props.name}>
             {props.defaultValue}
           </textarea>
         ) : (
-          <input {...props} className={className} />
+          <input {...props} />
         )}
       </label>
     </div>
