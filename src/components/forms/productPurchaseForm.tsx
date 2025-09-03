5565c3764ff7998d172e34252c73061dac5af0fc
Manuel <github.splurge944@passinbox.com>
2025-09-03 18:57:59 -0500

    feat: add product purchase form

---
+++ src/components/forms/productPurchaseForm.tsx
@@ -0,0 +1,25 @@
+import { CompraProducto } from "@/generated/prisma";
+import GenericFormProps from "@/interfaces/genericFormProps";
+import FormWrapper from "./formWrapper";
+import InputField from "../atoms/InputField";
+import PriceField from "../molecules/priceField";
+import DropDownField from "../atoms/dropDownField";
+import productsRepo from "@/repositories/productsRepository";
+
+export default async function ProductPurchaseForm({ entity, action }: GenericFormProps<CompraProducto>) {
+  const products = await productsRepo.getAll();
+
+  return (
+  <FormWrapper action={action}>
+    <InputField label="Fecha" type="date" name="fecha" />
+    <DropDownField label="Producto" name="id_producto">
+      <option value="0">- Seleccione -</option>
+      {products.map(product => (
+        <option value={product.id}>{product.nombre}</option>
+      ))}
+    </DropDownField>
+    <InputField label="cantidad" type="number" name="cantidad" />
+    <PriceField label="Valor total" name="valor_total"/>
+  </FormWrapper>
+  );
+}
