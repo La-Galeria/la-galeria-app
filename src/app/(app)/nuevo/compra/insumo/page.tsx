80c3598c8993b92187684ad02d3da239b190f537
Manuel <github.splurge944@passinbox.com>
2025-09-06 19:02:21 -0500

    feat: add assetPurchaseForm to creation page

---
+++ src/app/(app)/nuevo/compra/insumo/page.tsx
@@ -0,0 +1,11 @@
+import createAssetPurchase from "@/actions/assetPurchaseActions/createAssetPurchase";
+import AssetPurchaseForm from "@/components/forms/assetPurchaseForm";
+import FormCard from "@/components/molecules/formCard";
+
+export default function Page() {
+  return (
+    <FormCard title="Nueva compra">
+      <AssetPurchaseForm action={createAssetPurchase} />
+    </FormCard>
+  );
+}
