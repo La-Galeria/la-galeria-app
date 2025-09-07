ac888eab0dd012f0a8fd730e8d98a97f180ec271
Manuel <github.splurge944@passinbox.com>
2025-09-06 19:28:05 -0500

    feat: add value calculation in service and product sale actions

--- src/actions/productSaleActions/createProductSale.ts
+++ src/actions/productSaleActions/createProductSale.ts
@@ -2,13 +2,23 @@
 
 import ProductSaleDTO from "@/dtos/productSaleDto";
 import productSalesRepo from "@/repositories/productSalesRepository";
-import { redirect } from "next/navigation";
+import productsRepo from "@/repositories/productsRepository";
+import { notFound, redirect } from "next/navigation";
 
 export default async function createProductSale(formData: FormData) {
+  const productId = Number(formData.get("id_producto"));
+  const product = await productsRepo.get(productId);
+  if (!product) {
+    return notFound();
+  }
+
+  const amount = Number(formData.get("cantidad"));
+
   const sale: ProductSaleDTO = {
     fecha: new Date(String(formData.get("fecha"))),
-    cantidad: Number(formData.get("cantidad")),
-    idProducto: Number(formData.get("id_producto")),
+    cantidad: amount,
+    idProducto: productId,
+    valor: product.precioVenta * amount
   };
 
   await productSalesRepo.add(sale);
--- src/actions/serviceSaleActions/createServiceSale.ts
+++ src/actions/serviceSaleActions/createServiceSale.ts
@@ -2,13 +2,23 @@
 
 import ServiceSaleDTO from "@/dtos/serviceSaleDto";
 import serviceSalesRepo from "@/repositories/serviceSalesRepository";
-import { redirect } from "next/navigation";
+import servicesRepo from "@/repositories/servicesRepository";
+import { notFound, redirect } from "next/navigation";
 
 export default async function createServiceSale(formData: FormData) {
+  const serviceId = Number(formData.get("id_servicio"));
+  const service = await servicesRepo.get(serviceId);
+  if (!service) {
+    return notFound();
+  }
+
+  const amount = Number(formData.get("cantidad"));
+
   const sale: ServiceSaleDTO = {
     fecha: new Date(String(formData.get("fecha"))),
-    cantidad: Number(formData.get("cantidad")),
-    idServicio: Number(formData.get("id_servicio")),
+    cantidad: amount,
+    idServicio: serviceId,
+    valor: service?.precio
   };
 
   await serviceSalesRepo.add(sale);
