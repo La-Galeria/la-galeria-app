0c6adc798bf3672586fc24e1420132400e2ef71d
Manuel <github.splurge944@passinbox.com>
2025-09-22 19:19:12 -0500

    feat: add deleteService as deleteHandler in the servicios/[id] page

--- src/app/(app)/servicios/[id]/page.tsx
+++ src/app/(app)/servicios/[id]/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { deleteService } from "@/actions/serviceActions";
 import ElementView from "@/components/molecules/elementView";
+import { useParams } from "next/navigation";
 
 export default function Page() {
-  return <ElementView name="Corte" price={10000} />
+  const { id } = useParams();
+  return <ElementView name="Corte" price={10000} deleteHandler={deleteService} />
 }
